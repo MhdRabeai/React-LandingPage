8c9f5cc2cda0e318592dea6130c9ef0977dcefe4
Mhd.Rabeai <mhd.rabea.naser@gmail.com>
2024-06-13 16:15:55 +0300

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return <nav>
+    <div className="nav-logo">EV-olution</div>
+    <ul className="nav-menu">
+      <li>Home</li>
+      <li>Explore</li>
+      <li>About</li>
+      <li className="nav-contact">Contact</li>
+    </ul>
+  </nav>;
+};
